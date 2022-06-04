4262d4ddaf193174da0c648fdccc0e3a7828690b
kelvinachiando <kelvinostin22gmail.com>
2022-06-04 15:39:03 +0300

    commit tree 38b72937f50e39ec31affe769a84aba95cf339e7

--- index.js
+++ index.js
@@ -7,22 +7,22 @@ function subtract (a, b){
 }
 
 function multiply (a, b){
-    return a*b
+    return a*b;
 }
 
 function divide(a, b){
-    return a/b
+    return a/b;
 
 }
 function increment (n){
-    return (n+=1)
+    return (n+=1);
 }
 function decrement (n){
-    return (n-=1)
+    return (n-=1);
 }
 function makeInt(n){
-    return parseInt(n, 10)
+    return parseInt(n, 10);
 }
 function preserveDecimal(n){
-    return parseFloat(n)
+    return parseFloat(n);
 }
